ead1500f2219acdc3c1e67cd9bfe3893af36252b
Blazej Doleska <blazej.doleska@creativestyle.pl>
2018-11-27 16:00:51 +0100

    init commit

---
+++ DataProviders/AdditionalWarmupUrls.php
@@ -0,0 +1,12 @@
+<?php
+namespace MageSuite\WarmupAdditionalUrls\DataProviders;
+
+class AdditionalWarmupUrls implements \MageSuite\PageCacheWarmer\DataProviders\AdditionalWarmupUrlsInterface
+{
+    public function getAdditionalUrls()
+    {
+        return [
+            'ddddd33344445555'
+        ];
+    }
+}
